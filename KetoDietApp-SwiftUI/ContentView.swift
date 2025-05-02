467df597e1198df8f18e0216869a249b6a352d2b
gneinaz123 <gneinazreen3@gmail.com>
2025-05-02 13:45:50 +0800

    Initial Commit

---
+++ KetoDietApp-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KetoDietApp-SwiftUI
+//
+//  Created by user271428 on 5/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KetoDietApp-SwiftUI/KetoDietApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KetoDietApp_SwiftUIApp.swift
+//  KetoDietApp-SwiftUI
+//
+//  Created by user271428 on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct KetoDietApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
