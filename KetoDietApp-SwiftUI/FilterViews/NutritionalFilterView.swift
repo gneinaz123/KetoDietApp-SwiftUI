b81f4a6c17995b80f451b7c63d927b1e7203169b
gneinaz123 <gneinazreen3@gmail.com>
2025-05-03 22:31:36 +0800

    toggle button for addng filters & removing

--- KetoDietApp-SwiftUI/FilterViews/NutritionalFilterView.swift
+++ KetoDietApp-SwiftUI/FilterViews/NutritionalFilterView.swift
@@ -11,79 +11,96 @@ struct NutritionalFilterView: View {
     @StateObject private var viewModel = NutritionalFilterViewModel()
     var body: some View {
         NavigationView{
-//            ScrollView{
-            VStack(alignment: .leading, spacing: 16){
-                VStack(alignment: .leading, spacing: 12){
-                    Text("Customize your nutritional goals")
-                        .font(.headline)
-                    
-                    Picker("Select Category", selection: $viewModel.selectedCategory){
-                        ForEach(NutrientCategory.allCases) { category in
-                            Text(category.rawValue).tag(category)
-                        }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                    
-                    HStack{
-                        TextField("Min (g)", text: $viewModel.minAmount)
-                            .keyboardType(.numberPad)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
+            ScrollView{
+                VStack(alignment: .leading, spacing: 16){
+                    VStack(alignment: .leading, spacing: 12){
+                        Text("Customize your nutritional goals")
+                            .font(.headline)
                         
-                        TextField("Max (g)", text: $viewModel.maxAmount)
-                            .keyboardType(.numberPad)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        
-                        Button("Add"){
-                            viewModel.addFilter()
-                        }
-                        .buttonStyle(.borderedProminent)
-                    }
-//                    if !viewModel.filters.isEmpty{
-                    VStack(alignment: .leading){
-                        Text("Active Filters")
-                            .font(.subheadline)
-                        ForEach(viewModel.filters) { filter in
-                            HStack{
-                                Text("\(filter.category.rawValue): \(filter.min) - \(filter.max)g")
-                                Spacer()
-                                Button("Remove"){
-                                    viewModel.removeFilters(filter)
-                                }
-                                .foregroundColor(.red)
-                                
+                        Picker("Select Category", selection: $viewModel.selectedCategory){
+                            ForEach(NutrientCategory.allCases) { category in
+                                Text(category.rawValue).tag(category)
                             }
                         }
-//                        Button("Search Recipes"){
-//                            viewModel.searchRecipes()
-//                        }
-//                        .padding(.top)
+                        .pickerStyle(SegmentedPickerStyle())
+                        
+                        HStack{
+                            TextField("Min (g)", text: $viewModel.minAmount)
+                                .keyboardType(.numberPad)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+                            TextField("Max (g)", text: $viewModel.maxAmount)
+                                .keyboardType(.numberPad)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+                            Button("Add"){
+                                viewModel.addFilter()
+                            }
+                            .buttonStyle(.borderedProminent)
+                        }
+                        if !viewModel.filters.isEmpty{
+                            VStack(alignment: .leading){
+                                Text("Active Filters")
+                                    .font(.subheadline)
+//                                List{
+                                    ForEach(viewModel.filters) { filter in
+                                        HStack{
+                                            Toggle(isOn: .constant(true)){
+                                                Text("\(filter.category.rawValue): \(filter.min) - \(filter.max)g")
+                                            }
+                                            .disabled(true)
+                                            Spacer()
+                                            Button("Remove"){
+                                                viewModel.removeFilters(filter)
+                                            }
+                                            .foregroundColor(.red)
+                                            
+                                        }
+                                    }
+                                }
+                                
+                                
+                                //                        Button("Search Recipes"){
+                                //                            viewModel.searchRecipes()
+                                //                        }
+                                //                        .padding(.top)
+                            }
+                            
+                            Button("Search Recipes"){
+                                viewModel.searchRecipes()
+                            }
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .clipShape(Capsule())
+                            
+                        }
+                        
+                        if viewModel.isLoading{
+                            ProgressView("Loading...")
+                        } else{
+                            VStack(alignment: .leading, spacing: 10){
+                                ForEach(viewModel.recipes){ recipe in
+                                    RecipeCard(recipe: recipe)
+                                }
+                            }
+                            
+                            .padding(.top)
+                            
+                        }
                     }
-                    Button("Search Recipes"){
-                        viewModel.searchRecipes()
-                    }
-                    .padding(.top, 8)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.green)
-                    .foregroundColor(.white)
-                    .clipShape(Capsule())
-                   
+                    .padding()
+                
+                    
                 }
-                .padding()
-                if viewModel.isLoading{
-                    ProgressView("Loading...")
-                } else{
-                    List(viewModel.recipes){ recipe in
-                        RecipeCard(recipe: recipe)
-                    }
-                }
+                .navigationTitle("Nutritional Filter")
                 
             }
-            .navigationTitle("Nutritional Filter")
             
         }
-        
     }
-}
+
 
 #Preview {
     NutritionalFilterView()
